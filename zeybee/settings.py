337886978e20ebdf557eb6888939752095d22b0e
Diribo Hassan <diribohassan@gmail.com>
2020-09-25 02:54:16 +0300

    ettings.py edited

--- zeybee/settings.py
+++ zeybee/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'bridal.apps.BridalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
